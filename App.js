a4cee073f4b91d154f1e443e432bd64a6ecfac7a
VIKASH251204 <vikashtailor2512@gmail.com>
2025-03-02 12:46:28 +0530

    files pload

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './componets/Navbar'
+import About from './componets/About';
+import Home from './componets/Home';
+import Skill from './componets/Skill';
+import Pro from './componets/Pro';
+import Project from './componets/Project';
+import Contact from './componets/Contact';
+import Footer from './componets/Footer';
+
+
+
+
+
+function App() {
+  return (
+    <>   
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Pro/>
+    <Skill/>
+    <Project/>
+    <Contact/>
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App;
